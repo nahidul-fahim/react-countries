861be2b6eee24459d6c7035928f1a26864e10c40
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-09-10 19:49:32 +0600

    added some styles and more information

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,13 @@
 import './Country.css'
 
 const Country = ({country}) => {
-    const {name, flags} = country;
+    const {name, flags, capital, population} = country;
     return (
         <div className='country'>
-            <h3>Name: {name?.common} </h3>
-            <img src={flags?.png} alt="" />
+            <h2 className='country-name'>{name?.common} </h2>
+            <h4 className='details'><span className='keys-style'>Capital City:</span> {capital} </h4>
+            <h4 className='details'> <span className='keys-style'>Population:</span> {population} </h4>
+            <img className='flag' src={flags?.png} alt="" />
         </div>
     );
 };
