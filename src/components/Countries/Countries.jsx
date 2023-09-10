c53e9a9ad22106eae1264c7534e9f03b7fda8381
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-09-10 23:18:47 +0600

    added more react components

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css';
 
 const Countries = () => {
 
@@ -8,16 +9,18 @@ const Countries = () => {
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    } , [])
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
 
     return (
         <div>
-            <h3>Countries: {countries.length}</h3>
-            {
-               countries.map(country => <Country key = {country.cca3} country = {country} > </Country>)
-            }
+            <h2>Countries: {countries.length}</h2>
+            <div className="country-container">
+                {
+                    countries.map(country => <Country key={country.cca3} country={country} > </Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,14 @@
 import './Country.css'
 
 const Country = ({country}) => {
-    const {name, flags, capital, population} = country;
+    const {name, flags, capital, population, cca3} = country;
     return (
         <div className='country'>
-            <h2 className='country-name'>{name?.common} </h2>
+            <h2 className='country-name'>{name?.common} <span className='country-code'>({cca3})</span> </h2>
             <h4 className='details'><span className='keys-style'>Capital City:</span> {capital} </h4>
             <h4 className='details'> <span className='keys-style'>Population:</span> {population} </h4>
             <img className='flag' src={flags?.png} alt="" />
+            <button className='primary-btn'>Visit Status</button>
         </div>
     );
 };
