00bfba48fe6ea3228f8ef87b0ba6416d1251a34b
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-09-11 11:12:30 +0600

    created visited country function

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -6,19 +6,24 @@ import './Countries.css';
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => setCountries(data))
-    }, [])
+    }, []);
+
+    const hadleVisitedCountry = country => {
+        console.log(country);
+    };
 
     return (
         <div>
             <h2>Countries: {countries.length}</h2>
             <div className="country-container">
                 {
-                    countries.map(country => <Country key={country.cca3} country={country} > </Country>)
+                    countries.map(country => <Country key={country.cca3} hadleVisitedCountry={hadleVisitedCountry} country={country} > </Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country, hadleVisitedCountry}) => {
     const {name, flags, capital, population, cca3} = country;
 
     const [visited, setVisited] = useState(false);
 
     const handleVisited = () => {
         setVisited(!visited);
-    }
-
-
+    };
+ 
     return (
         <div className='country'>
             <h2 className='country-name'>{name?.common} <span className='country-code'>({cca3})</span> </h2>
             <h4 className='details'><span className='keys-style'>Capital City:</span> {capital} </h4>
             <h4 className='details'> <span className='keys-style'>Population:</span> {population} </h4>
             <img className='flag' src={flags?.png} alt="" />
+            <button onClick={() => hadleVisitedCountry (country.name?.common)} className='primary-btn'>Mark Visited</button>
             <button onClick={handleVisited} className= {visited ? 'visited-btn' : 'not-visited-btn'} >Visit Status!</button>
         </div>
     );
