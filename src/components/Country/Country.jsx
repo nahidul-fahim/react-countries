515e53025d0e447f557471658cf22b56ef457b35
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-09-11 09:23:28 +0600

    added visited or not visited functionality

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import './Country.css'
 
 const Country = ({country}) => {
     const {name, flags, capital, population, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
+
     return (
         <div className='country'>
             <h2 className='country-name'>{name?.common} <span className='country-code'>({cca3})</span> </h2>
             <h4 className='details'><span className='keys-style'>Capital City:</span> {capital} </h4>
             <h4 className='details'> <span className='keys-style'>Population:</span> {population} </h4>
             <img className='flag' src={flags?.png} alt="" />
-            <button className='primary-btn'>Visit Status</button>
+            <button onClick={handleVisited} className= {visited ? 'visited-btn' : 'not-visited-btn'} >Visit Status!</button>
         </div>
     );
 };
